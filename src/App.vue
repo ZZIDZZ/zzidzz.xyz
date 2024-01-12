ed4e72b24439610334744bb2f3672160eae8d401
zidan utomo <zidanutomo@gmail.com>
2024-01-12 14:10:37 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,6 @@
     </div>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://brobin.github.io/hacker-bootstrap/css/hacker.css">
 
     <nav>
       <div class="container">
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import './assets/bootstrap.min.css'
+import './assets/hacker.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
